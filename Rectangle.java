be54cd6376ee85f061240764d87d1f7583e52726
AKIMANIZANYEChantal <akimanizanyechantal11@gmail.com>
2025-09-26 01:47:23 -0700

    19september 2025

---
+++ Rectangle.java
@@ -0,0 +1,26 @@
+package Com.Shape;
+
+public class Rectangle {
+	private double length;
+	private double width;
+	
+	//constructor
+	public Rectangle(double length, double width) {
+		super();
+		this.length = length;
+		this.width = width;
+	}
+	
+	public double calArea(){
+		return this.length*this.width;
+	}
+	public double calPerimeter(){
+		return (this.length+this.width)*2;
+	}
+	
+	
+
+	
+
+
+}
